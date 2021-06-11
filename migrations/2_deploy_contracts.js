511292f715dc63ca86a0cfbceea252db3bd4c141
Mike Prince <mike@mikeprince.com>
2021-06-11 14:58:59 -0700

    Added Ferrum anti-bot to ERC20

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,9 @@ const OpulousTokenVesting = artifacts.require("OpulousTokenVesting");
 
 module.exports = async function(deployer) {
     await deployer.deploy( OpulousToken, 100000000 ); // 100M tokens
-    await deployer.deploy( OpulousTokenVesting, OpulousToken.address, {
-    	gas: 15000000,			// 15M current limits 
-    	gasPrice: 50000000000	// gwei
-    });
+    await deployer.deploy( OpulousTokenVesting, OpulousToken.address );
+    // {
+    	//gas: 15000000,			// 15M current limits 
+    	//gasPrice: 50000000000	// gwei
+    //});
 };
--- truffle-config.js
+++ truffle-config.js
@@ -48,7 +48,7 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
-      //from: ''
+      gas: 4500000,
     },
     // Another network with more advanced options...
     // advanced: {
