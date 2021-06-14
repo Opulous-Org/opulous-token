8e464689654526f1a0e0910acf4a7c5053117bd8
Mike Prince <mike@mikeprince.com>
2021-06-14 13:06:14 -0700

    Added burnable to token contract

--- contracts/OpulousToken.sol
+++ contracts/OpulousToken.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface ILocker {
@@ -14,7 +15,7 @@ interface ILocker {
      returns (bool, uint256);
 }
 
-contract OpulousToken is ERC20, Ownable {
+contract OpulousToken is ERC20, ERC20Burnable, Ownable {
 
     ILocker public locker;
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,6 @@ const OpulousToken = artifacts.require("OpulousToken");
 const OpulousTokenVesting = artifacts.require("OpulousTokenVesting");
 
 module.exports = async function(deployer) {
-    await deployer.deploy( OpulousToken, 100000000 ); // 100M tokens
+    await deployer.deploy( OpulousToken, 100000000 ); // 100M tokens x 10^18
     await deployer.deploy( OpulousTokenVesting, OpulousToken.address );
-    // {
-    	//gas: 15000000,			// 15M current limits 
-    	//gasPrice: 50000000000	// gwei
-    //});
 };
